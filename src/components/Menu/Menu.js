421ad711e5bc2d527cc42f9c64ab96c80d8ff99b
Jonathan <jonathan.maglianorx@gmail.com>
2023-08-22 22:11:41 -0300

    Updates

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar, NavItem } from 'react-materialize'
 import './Menu.scss'
 
-const Menu = () => (
-    <div className='menu'>
-        <a href='https://jonathanmagliano.github.io/johntor' className='menu__logo'>JohnTor</a>
-        <Navbar className='menu__links'>
-            <NavItem className='menu__links__logo'>
-                <a href='https://jonathanmagliano.github.io/johntor'>JohnTor</a>
-            </NavItem>
-            <NavItem className='menu__links__item'>
-                <Link data-toggle="collapse" data-target=".sidenav" to='/solucoes'>Soluções</Link>
-            </NavItem>
-            <NavItem className='menu__links__item'>
-                <Link to='/sobre'>Sobre</Link>
-            </NavItem>
-        </Navbar>
-    </div>
-)
+const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default Menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+      <a href='https://jonathanmagliano.github.io/johntor' className='menu__logo'>JohnTor</a>
+      <Navbar className='menu__links'>
+        <NavItem className='menu__links__logo'>
+          <a href='https://jonathanmagliano.github.io/johntor'>JohnTor</a>
+        </NavItem>
+        <NavItem className='menu__links__item'>
+          <Link data-toggle='collapse' data-target='.sidenav' to='/solucoes' onClick={closeMenu}>Soluções</Link>
+        </NavItem>
+        <NavItem className='menu__links__item'>
+          <Link to='/sobre' onClick={closeMenu}>Sobre</Link>
+        </NavItem>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Menu;
